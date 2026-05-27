e1d884c0b4a6086be7c4f339337948afa7d99c10
Logiqx <software@mikeg.me.uk>
2026-05-27 10:51:57 +0100

    List APPro models which are not on these pages

--- bin/appro.sh
+++ bin/appro.sh
@@ -9,3 +9,8 @@ for DEVICE in $(cut -d'"' -f2 $IN)
 do
   jq -r '.deviceId + "," + .displayName + "," + (.partNumbers[] | .number + "," + .connectIQVersion) + "," + .displayType + "," + .deviceFamily + "," + .deviceGroup' $DEVICES/$DEVICE/compiler.json
 done >$OUT
+
+for MODEL in $(cut -d, -f3 $OUT | sed 's/006-B//;s/-00//')
+do
+  echo $MODEL:$(cat docs/devices/garmin/watches/*md | grep -c $MODEL)
+done | grep :0 | sort -n
