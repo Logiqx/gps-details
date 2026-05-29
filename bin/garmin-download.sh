3083e410365552509b87d424bacae8da85773be1
Logiqx <software@mikeg.me.uk>
2026-05-29 09:26:04 +0100

    Sort Garmin data by part number

--- bin/garmin-download.sh
+++ bin/garmin-download.sh
@@ -12,10 +12,10 @@ jq . $TMP | sed 's/™//;s/®//' >$OUT
 rm $TMP
 
 CSV=$DATA_DIR/deviceTypes.csv
-jq -r '.[] | (.partNumber + "," + .name)' $OUT | sort -f -t, -k2 >$CSV
+jq -r '.[] | (.partNumber + "," + .name)' $OUT | sort >$CSV
 
 TXT=$DATA_DIR/deviceTypes.txt
-sed -E 's/(006-B)(....)(-00),(.*)/| \2 | \4 | \1\2\3 |/' $CSV >$TXT
+sort -f -t, -k2 $CSV | sed -E 's/(006-B)(....)(-00),(.*)/| \2 | \4 | \1\2\3 |/' >$TXT
 
 CSV=$DATA_DIR/deviceTypesWide.csv
-jq -r '.[] | [.partNumber, .name] +  [.additionalNames[]] | join(",")' $OUT | sort -f -t, -k2 >$CSV
+jq -r '.[] | [.partNumber, .name] +  [.additionalNames[]] | join(",")' $OUT | sort >$CSV
